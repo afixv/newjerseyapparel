d487249124ca381007c19b266a21ad3137dc279b
afixv <afixv00@gmail.com>
2024-04-17 08:52:58 +0700

    feat: add routes for the CRUD method

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\PortfolioController;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::post('/send-message', [OrderController::class, 'sendMessage'])->name('send-message');
+
+Route::resource('portfolios', PortfolioController::class);
+Route::resource('products', ProductController::class);
+Route::resource('services', ServiceController::class);
+
+
